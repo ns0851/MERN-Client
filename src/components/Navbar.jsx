f606d3974f86dd3467c57e6a4748a5fb7a8c53af
Satyadev <126260629+ns0851@users.noreply.github.com>
2024-10-22 09:13:58 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-col md:flex-row gap-5 md:gap-20 items-center w-full bg-slate-200 sticky top-0 p-5">
+    <nav className="flex flex-col md:flex-row gap-5 md:gap-20 items-center w-full bg-slate-200 p-5">
         <div className="text-2xl md:text-4xl font-bold">STORE</div>
         <div className="flex flex-col md:flex-row list-none gap-3 md:gap-6 text-lg md:text-2xl text-gray-500 font-normal">
             
