47beea7e56f43d44e66b698dd0181fe9e073af36
Satyadev <126260629+ns0851@users.noreply.github.com>
2024-10-22 09:10:58 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="flex gap-20 items-center bg-slate-200 sticky top-0">
-        <div className="text-4xl font-bold p-5">STORE</div>
-        <div className="flex list-none gap-6 text-2xl text-gray-500 font-normal">
+    <nav className="flex flex-col md:flex-row gap-5 md:gap-20 items-center w-full bg-slate-200 sticky top-0 p-5">
+        <div className="text-2xl md:text-4xl font-bold">STORE</div>
+        <div className="flex flex-col md:flex-row list-none gap-3 md:gap-6 text-lg md:text-2xl text-gray-500 font-normal">
             
-        <NavLink to='/' className={(e)=>{return e.isActive?'font-medium':''}}><li className=" p-5">Create Post</li></NavLink>
-        <NavLink to='/all' className={(e)=>{return e.isActive?'font-medium':''}}><li className=" p-5">All Posts</li></NavLink>
+        <NavLink to='/' className={(e)=>{return e.isActive?'font-medium':''}}><li className="p-2 md:p-5">Create Post</li></NavLink>
+        <NavLink to='/all' className={(e)=>{return e.isActive?'font-medium':''}}><li className="p-2 md:p-5">All Posts</li></NavLink>
             
         </div>
     </nav>
